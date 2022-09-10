8713492f7517d6f1163c7d3e86804a1081e88560
Nino Annighoefer <nino@ninoan.com>
2022-09-10 20:55:43 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
 module.exports = {
-  purge: {
-    // Specify the paths to all of the template files in your project
-    content: [
-      './src/components/**/*.res',
-      './src/layouts/**/*.res',
-      './src/*.res',
-    ],
-    options: {
-      safelist: ["html", "body"],
-    }
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-  },
-  variants: {
-  },
-  plugins: []
-}
+  content: [
+    "./src/components/**/*.res",
+    "./src/layouts/**/*.res",
+    "./src/*.res",
+  ],
+};
